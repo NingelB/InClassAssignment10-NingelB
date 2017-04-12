d3b53de102a6d24e86e16a4b8d1e3891f4ab2531
cmltdstudent <nb2748@tc.columbia.edu>
2017-04-12 15:45:23 -0400

    New Project

---
+++ app/src/main/java/com/example/cmltdstudent/inclassassignment10_ningelb/KEYS.java
@@ -0,0 +1,11 @@
+package com.example.cmltdstudent.inclassassignment10_ningelb;
+
+/**
+ * Created by cmltdstudent on 4/10/17.
+ */
+
+public interface KEYS {
+
+    String MONUMENTS = "monuments";
+
+}
